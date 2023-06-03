51ae554bc7378a5465dc658ab3c75e3fd76e1f66
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-03 14:22:40 +0100

    done

--- CALCULATOR/CALCULATOR/urls.py
+++ CALCULATOR/CALCULATOR/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
 
@@ -23,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Maths_Equations.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
