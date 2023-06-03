2409773de66fb3205f7086d2c603dbd1654732dd
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-03 01:03:27 +0100

    'almost'

--- CALCULATOR/CALCULATOR/settings.py
+++ CALCULATOR/CALCULATOR/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+django_heroku.settings(locals())
 
 
 STATIC_URL = 'static/'
