3eb8a1b82002adc504d03945e15f652916e86e70
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-04 16:31:50 +0100

    done

--- CALCULATOR/CALCULATOR/urls.py
+++ CALCULATOR/CALCULATOR/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 
     
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL , document_root=settings.STATICFILES_DIR)
